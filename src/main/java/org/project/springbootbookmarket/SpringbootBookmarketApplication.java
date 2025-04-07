f5942d6c47c20fa6ad01f2bb84626d11243d1b51
Hyeonbin2379 <hyeonbin2379@gmail.com>
2025-04-07 14:57:26 +0900

    init : 초기 생성

---
+++ src/main/java/org/project/springbootbookmarket/SpringbootBookmarketApplication.java
@@ -0,0 +1,13 @@
+package org.project.springbootbookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBookmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBookmarketApplication.class, args);
+    }
+
+}
